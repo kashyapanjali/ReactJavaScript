41ef1b28a7066b871b30adc4fdd2c996a06d1956
kashyapanjali <anjalikashyap9608@gmail.com>
2024-08-18 04:03:02 +0530

    login_credential function

--- amazon_clone/src/App.js
+++ amazon_clone/src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Checkout from "./Checkout.js";
 import Header from "./Header.js";
 import Home from "./Home.js";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Login.js";
+import { auth } from "./firebase.js";
 
 function App() {
+  useEffect(() => {
+    //will only run once when the app components loads.....
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>>", authUser);
+    });
+    if (authUser) {
+      //user just loggged in / the user was logged in
+    } else {
+      //the user is logged out
+    }
+  }, []);
+
   return (
     <Router>
       <div className="app">
--- amazon_clone/src/Login.js
+++ amazon_clone/src/Login.js
@@ -1,8 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "./firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Successfully signed in
+        console.log(userCredential);
+        navigate("/");
+      })
+      .catch((error) => alert(error.message));
+    //some fancy firebase login shittttt.
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((auth) => {
+        //succesfully created a new user with email and password
+        console.log(auth);
+        if (auth) {
+          navigate("/");
+        }
+      })
+      .catch((error) => alert(error.message));
+
+    //do some fancy firebase register shitttt.
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -12,20 +49,32 @@ function Login() {
           alt=""
         />
       </Link>
-      {/* aove link for clickable router and go to home */}
+      {/* above link for clickable router and go to home */}
 
       <div className="login_container">
         <h1>Sign-in</h1>
         <form>
           <h5>E-mail</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <h5>Password</h5>
-          <input type="password" />
-          <button className="login_signInButton">Sign In</button>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          {/* for click the sign in button */}
+          <button type="submit" onClick={signIn} className="login_signInButton">
+            Sign In
+          </button>
         </form>
 
         <p>Amazon-clone</p>
-        <button className="login_registerButton">
+        <button onClick={register} className="login_registerButton">
           Create your Amazon Account
         </button>
       </div>
