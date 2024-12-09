e6e052584e0b141e416a2e5171c2336738c29cae
kashyapanjali <anjalikashyap9608@gmail.com>
2024-12-09 15:41:41 +0530

    api test

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -4,14 +4,20 @@ import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-const News = (props) => {
+const News = ({
+  setProgress,
+  apiKey,
+  country = "in",
+  pageSize = 8,
+  category = "general",
+}) => {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
   const [totalResults, setTotalResults] = useState(0);
 
   useEffect(() => {
-    document.title = `${capitalizeFirstLetter(props.category)} - NewsBundle`;
+    document.title = `${capitalizeFirstLetter(category)} - NewsBundle`;
     updateNews();
     // eslint-disable-next-line
   }, []);
@@ -21,25 +27,35 @@ const News = (props) => {
   };
 
   const updateNews = async () => {
-    props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?category=${category}&apiKey=${apiKey}&page=${page}&pageSize=${pageSize}`;
     setLoading(true);
-    let data = await fetch(url);
-    props.setProgress(30);
-    let parsedData = await data.json();
-    props.setProgress(70);
-    setArticles(parsedData.articles);
-    setTotalResults(parsedData.totalResults);
-    setLoading(false);
-    props.setProgress(100);
+    try {
+      let data = await fetch(url);
+      if (!data.ok) {
+        throw new Error(`HTTP error! status: ${data.status}`);
+      }
+      setProgress(30);
+      let parsedData = await data.json();
+      console.log("Fetched data:", parsedData);
+      if (parsedData.articles && parsedData.articles.length > 0) {
+        setArticles(parsedData.articles);
+        setTotalResults(parsedData.totalResults);
+      } else {
+        console.warn("No articles found in the response.");
+      }
+    } catch (error) {
+      console.error("Error fetching news:", error);
+    } finally {
+      setLoading(false);
+      setProgress(100);
+    }
   };
 
   const fetchMoreData = async () => {
-    const url = `https://newsapi.org/v2/top-headlines?country=${
-      props.country
-    }&category=${props.category}&apiKey=${props.apiKey}&page=${
+    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${
       page + 1
-    }&pageSize=${props.pageSize}`;
+    }&pageSize=${pageSize}`;
     setPage(page + 1);
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -53,9 +69,10 @@ const News = (props) => {
         className="text-center"
         style={{ margin: "35px 0px", marginTop: "90px" }}
       >
-        NewsBundle - Top {capitalizeFirstLetter(props.category)} Headlines
+        NewsBundle - Top {capitalizeFirstLetter(category)} Headlines
       </h1>
       {loading && <Spinner />}
+      {!loading && articles.length === 0 && <p>No news articles available.</p>}
       <InfiniteScroll
         dataLength={articles.length}
         next={fetchMoreData}
@@ -86,16 +103,12 @@ const News = (props) => {
   );
 };
 
-News.defaultProps = {
-  country: "in",
-  pageSize: 8,
-  category: "general",
-};
-
 News.propTypes = {
   country: PropTypes.string,
   pageSize: PropTypes.number,
   category: PropTypes.string,
+  setProgress: PropTypes.func.isRequired,
+  apiKey: PropTypes.string.isRequired,
 };
 
 export default News;
